10d7a2c2efb59163469904c438146a0fbc488fd6
marcinliwen <liwen.marcin@gmail.com>
2021-02-22 12:14:05 +0100

    no manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,28 +87,6 @@ module.exports = {
       }
     },
     {
-      plugins: [
-        {
-          resolve: `gatsby-plugin-manifest`,
-          options: {
-            name: "Kaja decor - Studio Dekoracji Okien ",
-            short_name: "Kaja decor",
-            start_url: "/",
-            background_color: "#2D2D2F",
-            theme_color: "#2D2D2F",
-            // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-            // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-            display: "standalone",
-            icon: "src/img/icon.png", // This path is relative to the root of the site.
-            // An optional attribute which provides support for CORS check.
-            // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-            // Any invalid keyword or empty string defaults to `anonymous`
-            //crossOrigin: `use-credentials`,
-          },
-        },
-      ]
-    },
-    {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
